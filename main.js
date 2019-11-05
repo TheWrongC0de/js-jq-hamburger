b5828ec76db8c7bf64d28e3ea5efd8e8687e28f2
Simone <simoneugazio1993@gmail.com>
2019-11-05 15:49:27 +0100

    Push

---
+++ main.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+
+    $(".fas.fa-bars").on({
+        click: function () {
+            $(".hamburger-menu").fadeIn();
+
+        },
+        mouseenter: function(){
+            $(".fas.fa-bars").css("color","blue");
+        },
+        mouseleave: function(){
+            $(".fas.fa-bars").css("color","inherit");
+        }
+
+    });
+
+    $(".fas.fa-times").on({
+        click: function () {
+            $(".hamburger-menu").fadeOut();
+
+        },
+        mouseenter: function () {
+            $(".fas.fa-times").css("color", "blue");
+        },
+        mouseleave: function () {
+            $(".fas.fa-times").css("color", "inherit");
+        }
+
+    });
+  });
